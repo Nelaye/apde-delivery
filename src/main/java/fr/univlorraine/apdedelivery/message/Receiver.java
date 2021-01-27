cffc3dc7dc05a87b02b203db6b819ad334de7e8a
Nelaye <emilien01lambert@gmail.com>
2021-01-27 20:01:41 +0100

    ajout dans le BDD de livraisons opérationnel

--- src/main/java/fr/univlorraine/apdedelivery/message/Receiver.java
+++ src/main/java/fr/univlorraine/apdedelivery/message/Receiver.java
@@ -8,7 +8,11 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.sql.Date;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @RabbitListener(queues = "delivery")
 @Component
@@ -21,6 +25,13 @@ public class Receiver {
     public void receive(String in) {
         System.out.println(" [x] Received : '" + in + "'");
         JSONObject order = new JSONObject(in);
-        dao.save(new Delivery(Integer.parseInt(order.getString("buyer")), Date.valueOf(order.getString("date")), order.getString("address"))); //create a new delivery entry
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Date date = null;
+        try {
+            date = df.parse(order.getString("date"));
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+        dao.save(new Delivery(Integer.parseInt(order.getString("buyer")), date, order.getString("address"))); //create a new delivery entry
     }
 }
